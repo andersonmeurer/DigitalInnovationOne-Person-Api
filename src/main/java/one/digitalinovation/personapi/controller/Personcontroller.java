52e7f3859d7ff190c8b14bfd2bd1427be84ef0fc
Anderson Meurer <anderson.meurer@gam.com.br>
2021-09-17 13:55:13 -0300

    App rodando!

--- src/main/java/one/digitalinovation/personapi/controller/Personcontroller.java
+++ src/main/java/one/digitalinovation/personapi/controller/Personcontroller.java
@@ -1,20 +1,21 @@
 package one.digitalinovation.personapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import one.digitalinovation.personapi.service.PersonService;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/people")
 public class Personcontroller {
 
 	@Autowired
 	private PersonService service;
 
-	public ResponseEntity Personcontroller() {
-		return ResponseEntity.ok();
+	@GetMapping
+	public String Personcontroller() {
+		return "API Test!";
 	}
 }
