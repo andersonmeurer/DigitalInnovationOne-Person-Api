8e228e7765a02235dec7122d34f54612a46bdfa1
Anderson Meurer <andersonmeurerr@gmail.com>
2021-09-22 23:05:38 -0300

    Commit
    Controller, Service, Repositoty and MessageResponseDTO
    POST method to createPerson

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -2,13 +2,20 @@ package one.digitalinnovation.personapi.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.service.PersonService;
 
 @RestController
@@ -21,5 +28,12 @@ public class PersonController {
 	@GetMapping
 	public List<PersonDTO> listAll() {
 		return service.listAll();
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public MessageResponseDTO createPerson(@RequestBody @Valid 
+			PersonDTO personDto) {
+		return service.createPerson(personDto);
 	}
 }
--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
@@ -19,6 +20,17 @@ public class PersonService {
 	private PersonRepository repository;
 
 	private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
+	public MessageResponseDTO createPerson(PersonDTO personDto) {
+		Person person = personMapper.toModel(personDto);
+		return MessageResponseDTO
+		.builder()
+		.mensage("Created person with ID " + 
+				repository
+				.save(person)
+				.getId())
+		.build();
+	}
 
 	public List<PersonDTO> listAll() {
 		List<Person> list = repository.findAll();
